99a970577c78af4330eec9707e5b327f092c89d2
7zism3 <7zism3@gmail.com>
2025-09-17 20:01:28 +0700

    setup - update JavaVersion.VERSION_17

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -36,8 +36,8 @@ android {
     namespace = "com.sevenhallo.quickdrop.shared"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
